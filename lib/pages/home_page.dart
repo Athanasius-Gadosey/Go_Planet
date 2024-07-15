9a05076e3c838c08ec8888e704f373322c327d5c
A.J.K.G <athanasuisgadosey@gmail.com>
2024-07-15 22:19:40 +0000

    Column widget created

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,15 @@ class HomePage extends StatelessWidget {
           height: _deviceHeight,
           width: _deviceWidth,
           padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
-          child: _locationDropDownWidget(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _titlePage(),
+              _locationDropDownWidget(),
+            ],
+          ),
         ),
       ),
     );
